32662909fe70c00a55ed68cb6441e0b847bdf7ec
CodewithAbdullah2211 <148060391+CodewithAbdullah2211@users.noreply.github.com>
2024-02-11 15:14:05 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+// set map
+function initialize(){
+    var mapOptions = {
+        // zoom of map on start
+        zoom: 10,
+        // initial center cordinates on start (NewYork)
+        center: new google.maps.LatLng(40.7128, -74.0060),
+        // type of map (ROADMAP, SATELLITE, HYBRID, TERRAIN)
+        mapTypeId: google.maps.mapTypeId.ROADMAP,
+        // minimum zoom of map 
+        minZoom: 2
+    };
+
+
+    // Create a new map instance using provided options
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+    // Create an info window to display location info
+    var infoWindow = new google.maps.InfoWindow();
+
+    // Create a marker for example : Iran , Zanjan
+    var marker = new google.maps.Marker({
+        // Cordinates for Iran, Zanjan
+        position: new google.maps.LatLng(36.6769, 40.4963),
+        // Attach the marker
+        map: map,
+        // Tooltip on hover
+        title: 'Iran, Zanjan'
+    });
+
+    // Add click event listener for marker
+    marker.addListener('click', function(){
+        infoWindow.setContent(marker.title);
+        infoWindow.open(map, marker);
+    });
+
+    // Adjust map center when the window is resized 
+    google.maps.event.addDomListener(window, 'resize', function(){
+        map.setCenter(mapOptions.center);
+    })
+}
+
+// Initialize the map when window loading finished
+google.maps.event.addDomListener(window, 'load', initialize)
